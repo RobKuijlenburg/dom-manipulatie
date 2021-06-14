ee6e382cd808360bf21738803724b166ee462618
Rob Kuijlenburg <robkuijlenburg91@gmail.com>
2021-06-14 08:44:49 +0200

    html en js ingevoerd

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
+const naam = 'Rob';
+const colorArray = ['Red', 'Green', 'Purple', 'Blue', 'Yellow'];
+let randomColor = Math.floor(Math.random() * 5);
 
+
+function headerChange() {
+    const colorArray = ['Red', 'Green', 'Purple', 'Blue', 'Yellow'];
+    let randomColor = Math.floor(Math.random() * 5);
+    document.getElementById("hi").style.color = colorArray[randomColor];
+    document.getElementById("hi").innerHTML = "Hi " + naam;
+}
